b5d4a6319682aaadd73fca551735191781018bd0
Robertito99 <Robertito99@socorrista.com>
2024-02-07 10:00:17 +0100

    Commit Final

---
+++ Assets/Racket.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour{
+	public float speed = 150.0f;
+	
+	//FixedUpdate para Fisicas
+    void FixedUpdate(){
+    float horizontalDirection = Input.GetAxisRaw ("Horizontal");
+    
+    GetComponent<Rigidbody2D> ().velocity = Vector2.right * horizontalDirection * speed;
+        
+    }
+}
